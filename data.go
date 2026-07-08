aaad7444bd0823e1500721f357c6b6877c7614db
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-07-08 16:48:08 +0530

    Update data.go

--- data.go
+++ data.go
@@ -2,6 +2,6 @@
   "recording_file": "data/recording_001.sqlite",
   "target_url": "http://localhost:8080",
   "site_id": "b3489888-aacf-4451-893c-d7d994240f93",
-  "mock_mode": "sqlite",
-  "mock_site_file": ""
+  "mock_mode": "file",
+  "mock_site_file": "configs/wrong_site_config.json"
 }
