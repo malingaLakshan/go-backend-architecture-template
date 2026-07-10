da17143a15437e88b87502e60f7dcda00a791533
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-07-10 13:25:35 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,2 @@
-Select-String -Path .\internal\cli\args.go -Pattern "Config"
+Select-String -Path .\internal\cli\commands.go -Pattern "runMockServerWithConfig"
+Select-String -Path .\internal\cli\commands.go -Pattern "config.Load"
