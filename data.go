b84ab481f345ee1314c65f669fcd13ced64b7347
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-07-10 12:45:16 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,60 @@
-Yes, Pawarani. Since this is the initial versioning setup, it may take some time to complete properly. Once the initial versioning flow is done, we should be able to provide updated versions more easily for the next releases.
+{
+  "id": "WRONG-SITE-ID",
+  "name": "Wrong Site",
+  "readers": [
+    {
+      "id": "WRONG-READER-01",
+      "name": "Wrong Reader 01",
+      "type": "RFID",
+      "ipAddress": "192.168.99.101",
+      "floorId": "WRONG-FLOOR-01",
+      "x": 10,
+      "y": 20,
+      "antennas": [
+        {
+          "antenna_id": 99,
+          "antenna_type": 2,
+          "reader_id": "WRONG-READER-01",
+          "x": 10,
+          "y": 20
+        }
+      ]
+    }
+  ],
+  "floors": [
+    {
+      "id": "WRONG-FLOOR-01",
+      "name": "Wrong Floor",
+      "number": 0,
+      "width": 100,
+      "height": 100,
+      "regions": [
+        {
+          "id": "WRONG-REGION-01",
+          "name": "Wrong Region",
+          "type": "WRONG_TYPE",
+          "physicality": "VIRTUAL",
+          "inventoryType": "OTHER"
+        }
+      ]
+    }
+  ],
+  "regions": [
+    {
+      "id": "WRONG-REGION-01",
+      "name": "Wrong Region",
+      "type": "WRONG_TYPE",
+      "physicality": "VIRTUAL",
+      "inventoryType": "OTHER"
+    }
+  ],
+  "antennas": [
+    {
+      "antenna_id": 99,
+      "antenna_type": 2,
+      "reader_id": "WRONG-READER-01",
+      "x": 10,
+      "y": 20
+    }
+  ]
+}
