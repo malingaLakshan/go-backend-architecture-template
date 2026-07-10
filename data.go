64ee8c93fc276b8781eaaa939bf58e23b402b28b
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-07-10 15:49:40 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,34 +1,7 @@
-func Load(path string) (*RunConfig, error) {
-	data, configName, err := readAllowedConfigFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	var cfg RunConfig
-	if err := json.Unmarshal(data, &cfg); err != nil {
-		return nil, fmt.Errorf("failed to parse config file %s: %w", configName, err)
-	}
-
-	return &cfg, nil
-}
-
-func readAllowedConfigFile(path string) ([]byte, string, error) {
-	cleanPath := filepath.ToSlash(filepath.Clean(strings.TrimSpace(path)))
-
-	switch cleanPath {
-	case "configs/pass_config.json":
-		data, err := os.ReadFile("configs/pass_config.json")
-		return data, cleanPath, err
-
-	case "configs/fail_config.json":
-		data, err := os.ReadFile("configs/fail_config.json")
-		return data, cleanPath, err
-
-	case "configs/wrong_site_config.json":
-		data, err := os.ReadFile("configs/wrong_site_config.json")
-		return data, cleanPath, err
-
-	default:
-		return nil, "", fmt.Errorf("unsupported config file: %s", path)
+if len(os.Args) > 1 {
+	switch os.Args[1] {
+	case "version", "--version", "-v":
+		fmt.Printf("rre version %s\n", version)
+		return
 	}
 }
