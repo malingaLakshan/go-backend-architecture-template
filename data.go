4c921e7b59574fd2ce66cd4293dad1af6457f6ab
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-12 15:20:29 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,5 @@
-.\rr.exe validate-config --config ".\configs\config.json"
+"streams": {
+  "rawReadsUrl": "mqtt://117108-trirh901.117asd.zebra.lan:1883/resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/rawrfid",
+  "locationsUrl": "mqtt://117108-trirh901.117asd.zebra.lan:1883/resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/locationUpdate",
+  "eventsUrl": "mock://streams/events"
+},
