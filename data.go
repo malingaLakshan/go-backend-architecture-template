968a62856b9f48c3c2e6d4f51dfd16d2dd5ec816
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-12 12:50:06 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,9 @@
-sudo find /data/sim -maxdepth 3 -type f \( -name "*.proto" -o -name "messages_pb2.py" \) -print
+cd /data/sim
+./venv/bin/python3 - <<'PY'
+from google.protobuf import descriptor_pb2, text_format
+import messages_pb2
+
+schema = descriptor_pb2.FileDescriptorProto()
+messages_pb2.DESCRIPTOR.CopyToProto(schema)
+print(text_format.MessageToString(schema))
+PY
