c0b1a97431946cbfaaefbff9d1dd93c0c81c6183
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-12 10:57:01 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,1 @@
-sudo docker compose -f /opt/zebra/alt-resonate-locate/svc/docker-compose.yml config 2>&1 | grep -nEi 'generate.*event|rfid.?exit|pos.?event|event.*topic|topic.*event'
+sudo docker compose -f /opt/zebra/alt-resonate-locate/svc/docker-compose.yml config 2>&1 | grep -nEi -B 15 -A 20 'RFIDExitDistance'
