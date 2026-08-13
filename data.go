8e17f83267042520401ddcfb4056fd0509578179
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-13 10:33:00 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,5 +1,2 @@
-"streams": {
-  "rawReadsUrl": "mqtt://117108-trirh901.117asd.zebra.lan:1883/resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/rawrfid",
-  "locationsUrl": "mqtt://117108-trirh901.117asd.zebra.lan:1883/resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/locationUpdate",
-  "eventsUrl": "mock://streams/events"
-},
+Invoke-RestMethod "http://117108-trirh901.117asd.zebra.lan:3389/api/locations" |
+  ConvertTo-Json -Depth 10
