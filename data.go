05db4c48571adac49a543375075c8e12209e1b1b
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-18 15:06:27 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,2 +1,17 @@
-mosquitto_sub -h 127.0.0.1 -p 1883 \
--t 'resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/events/json' -v
+sudo docker exec mongodb mongosh \
+"mongodb://admin:admin@localhost:27017/location_engine?authSource=admin" \
+--quiet --eval '
+printjson(
+  db.feed_configs.find(
+    {},
+    {
+      _id: 0,
+      feedId: 1,
+      siteId: 1,
+      status: 1,
+      eventFilters: 1,
+      filterMode: 1,
+      destination: 1
+    }
+  ).toArray()
+)'
