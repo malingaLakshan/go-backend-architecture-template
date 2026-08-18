7ea9cc0f80f80d4c5c8465de4c2ba8c7fc899960
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-18 15:39:16 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,3 @@
-sudo docker cp alt-sl-datafeed-service:/app /tmp/datafeed-service-app
+strings /tmp/datafeed-service-app \
+| grep -iE 'mqtt://|tcp://|broker|mqtt.*publish|publish.*mqtt|eventFilters|filterMode' \
+| head -120
