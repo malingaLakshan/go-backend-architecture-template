6da2a68059889a06dbcb32b88c2143f44a4b9c59
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-19 10:25:07 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,2 +1,6 @@
-timeout 30 mosquitto_sub -h 127.0.0.1 -p 1883 -t '#' -F '%t' 2>/dev/null |
-awk '!seen[$0]++ {print; fflush()}'
+EVENT_TOPIC='resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/events/json'
+
+sudo docker exec mqtt emqx ctl trace start topic "$EVENT_TOPIC" /tmp/event-topic-check.log debug
+sleep 30
+sudo docker exec mqtt emqx ctl trace stop topic "$EVENT_TOPIC"
+sudo docker exec mqtt sh -c 'wc -c /tmp/event-topic-check.log; tail -40 /tmp/event-topic-check.log'
