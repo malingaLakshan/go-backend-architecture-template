b9470e811aa9b00b3d30ed2b85a3e8ddcc460ed7
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-19 11:48:36 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,2 @@
-sudo docker logs -f --since 10s alt-sl-datafeed-service 2>&1
+curl -sS -w '\nHTTP %{http_code}\n' -X POST \
+"http://localhost:3000/siteId('3b96f652-8200-3920-8a2c-0486c358964e')/Feeds/lab-json-events/Reset"
