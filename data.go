7565ca7e4d7e1f4fee53c020befea02e77e32bb8
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-19 11:42:59 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,6 +1,1 @@
-EVENT_TOPIC='resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/events/json'
-
-sudo docker exec mqtt emqx ctl trace start topic "$EVENT_TOPIC" /tmp/event-topic-check.log debug
-sleep 30
-sudo docker exec mqtt emqx ctl trace stop topic "$EVENT_TOPIC"
-sudo docker exec mqtt sh -c 'wc -c /tmp/event-topic-check.log; tail -40 /tmp/event-topic-check.log'
+sudo docker logs -f --since 10s alt-sl-datafeed-service 2>&1
