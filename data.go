a7f078b1c416a5f2e030740d18edcfe43c6e2473
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-21 11:02:27 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,2 +1,1 @@
-mosquitto_sub -h 117108-trirh902.117asd.zebra.lan -p 1883 \
--t 'resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/events/json' -v
+python3 -c "import paho.mqtt.client; print('Paho MQTT is available')"
