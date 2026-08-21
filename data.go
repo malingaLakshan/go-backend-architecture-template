8b7c653085dd997e1c208b89f203d872fb586304
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-21 10:59:23 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,2 @@
-curl -sS -w '\nHTTP %{http_code}\n' http://localhost:8080/sites
+mosquitto_sub -h 117108-trirh902.117asd.zebra.lan -p 1883 \
+-t 'resonate/locate/3b96f652-8200-3920-8a2c-0486c358964e/events/json' -v
