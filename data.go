df08ae239274668cd06ca52f831d34e179a54a97
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-24 10:30:51 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,7 +1,2 @@
-Good morning Steve,
-
-We successfully moved to trirh902 and received real Event JSON over MQTT using a test datafeed and configured destination topic.
-
-Could you please confirm the expected setup for Recorder and Validator? Should our team create, start and manage the Event datafeed and destination topic, or will Resonate provide a preconfigured datafeed and topic for both tools to use?
-
-Also, updating the original VM would be helpful for parallel Recorder and Validator testing.
+curl -sS -w '\nHTTP %{http_code}\n' -X POST \
+"http://localhost:3000/feed/siteId('3b96f652-8200-3920-8a2c-0486c358964e')/lab-json-events/Start"
