4255e5fd36c8861b6982eb4ee3d8d14c1502e5d2
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-25 10:37:03 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,5 +1,2 @@
-cd /opt/zebra/alt-resonate-locate/svc
-
-sudo docker cp "$(sudo docker compose ps -q api)":/app /tmp/smartlens-api
-
-strings /tmp/smartlens-api | grep -iE 'trifecta|sds/sites|sitegraph|/sites' | sort -u | head -100
+sudo grep -RIlE 'trifecta|sds/sites' \
+/opt/zebra/alt-resonate-locate 2>/dev/null
