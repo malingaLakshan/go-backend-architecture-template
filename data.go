1e2fd32ff8e78509c09aa95b29737e09f50485b8
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-25 10:19:21 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,9 +1,1 @@
-Hi Steve,
-
-We can access the SiteGraph for one site using:
-
-http://117108-trirh902.117asd.zebra.lan:3389/api/sitegraph/<site-id>
-
-However, we receive a CORS error when calling this API from our application.
-
-We also need an API to retrieve the complete site list before selecting one site. Could you please provide the correct site-list endpoint and advise how the CORS issue should be handled?
+sudo docker compose -f /opt/zebra/alt-resonate-locate/svc/docker-compose.yml logs --tail=100 api
