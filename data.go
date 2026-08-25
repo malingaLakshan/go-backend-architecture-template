f34c374ecbf54ba146dd9cdd1cb117fae69c9486
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-25 10:26:47 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,6 +1,2 @@
-cd /opt/zebra/alt-resonate-locate/svc
-
-sudo docker inspect "$(sudo docker compose ps -q api)" \
-  --format '{{range .Config.Env}}{{println .}}{{end}}' |
-  sort |
-  grep -iE 'url|host|port|site|sds|trifecta'
+sudo docker compose config |
+grep -iE 'url|host|port|site|sds|trifecta'
