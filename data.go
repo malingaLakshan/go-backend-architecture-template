bbe13438acae188b16d8ec3e097d6fa24ca624f2
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-25 10:31:58 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,2 +1,4 @@
-sudo docker compose config |
-grep -iE 'url|host|port|site|sds|trifecta'
+cd /opt/zebra/alt-resonate-locate/svc
+
+sudo docker inspect "$(sudo docker compose ps -q api)" \
+--format 'Path={{.Path}} Args={{json .Args}} WorkDir={{.Config.WorkingDir}}'
