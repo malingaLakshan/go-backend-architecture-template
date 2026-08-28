1344988a6dd0f37a5947d97344de293be73e3b01
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-08-28 12:20:22 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,1 +1,2 @@
-~/resonate-sim-venv/bin/python ~/event_stream_subscriber.py
+curl -sS -w '\nHTTP %{http_code}\n' -X POST \
+"http://localhost:3000/feed/siteId('3b96f652-8200-3920-8a2c-0486c358964e')/lab-json-events/Start"
