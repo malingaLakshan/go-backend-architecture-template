d08c6ec68b1c536b9bb13d1c703f6aae72bd4919
malingaLakshan <47074351+malingaLakshan@users.noreply.github.com>
2026-09-01 14:44:32 +0530

    Update data.go

--- data.go
+++ data.go
@@ -1,4 +1,7 @@
-Thank you, Ramesh. Please let me know a convenient time today, and we can sync to configure the datafeed and Event stream.Thank you, Steve. Yes, please proceed with updating the first VM with the latest images tomorrow morning and let us know once it is complete.
+Hi Astha,
 
+Yes, we need help with the Resonate Event stream. RawRFID and Location already have default MQTT topics, but Events do not have a default topic.
 
+We need the Resonate team to configure and start the required datafeed and provide us with the MQTT broker, port, and Event topic details. Our Recorder and Validator will only subscribe to that Event topic for testing.
 
+Could you please help us set this up on the test VM?
